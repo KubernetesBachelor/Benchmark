03c46b5876385e5aac26f67332442e7a43d30e3f
Mawa0 <94678889+Mawa0@users.noreply.github.com>
2024-05-10 15:06:45 +0200

    Update tabell.py

--- Sikkerhetsmekanismer/tabell.py
+++ Sikkerhetsmekanismer/tabell.py
@@ -2,6 +2,13 @@ import pandas as pd
 import numpy as np
 import os
 
+# Filnavn og etiketter for de fem datasettene
+csv_files = ['Standard.csv', 'PSA.csv', 'RBAC.csv', 'Auditing.csv', 'Herdet.csv']     # Erstatt '*.csv' filene med passende filnavn
+file_labels = ['Standard', 'PSA', 'RBAC', 'Auditing', 'Herdet']                       # Erstatt 'labelen' for å passe med filnavnene
+
+test_types = ['CPU', 'RAM', 'Disk']
+
+#Funksjon for å regne ut avvik i prosent og lage tabellen
 def calculate_percentage_differences(csv_files, file_labels, test_types, identifiers_dict, value_column):
     output_dir = 'percentage_differences'
     os.makedirs(output_dir, exist_ok=True)
@@ -10,7 +17,7 @@ def calculate_percentage_differences(csv_files, file_labels, test_types, identif
     for test_type in test_types:
         results = {}
         base_means = {}
-        identifiers = identifiers_dict[test_type]  # Velg riktig identifiers basert på testtypen
+        identifiers = identifiers_dict[test_type]
 
         for csv_file, label in zip(csv_files, file_labels):
             data = pd.read_csv(csv_file)
@@ -30,7 +37,7 @@ def calculate_percentage_differences(csv_files, file_labels, test_types, identif
                 if label == 'Standard':
                     base_means[key] = mean
 
-        # Beregne prosentforskjeller
+        # Beregner prosentforskjeller
         for key, labels_means in results.items():
             base_mean = base_means[key]
             for label, mean in labels_means.items():
@@ -42,25 +49,14 @@ def calculate_percentage_differences(csv_files, file_labels, test_types, identif
                     'Percentage Difference': percentage_diff
                 })
 
-    # Opprett en DataFrame fra samlet data
+    # Oppretter en DataFrame fra samlet data
     df = pd.DataFrame(all_data)
     df.to_csv(f'{output_dir}/all_percentage_differences.csv', index=False)
     print(f'Samlet tabell lagret: {output_dir}/all_percentage_differences1.csv')
 
-# Bruk av funksjonen
-csv_files = [
-    'Standard.csv',
-    'PSA.csv',
-    'RBAC.csv',
-    'Auditing.csv',
-    'Herdet.csv'
-]
-file_labels = ['Standard', 'PSA', 'RBAC', 'Auditing', 'Herdet']
-test_types = ['CPU', 'RAM', 'Disk']
-
 identifiers_dict = {
     'CPU': ['Mode/Threads', 'cpu-max-prime (Only CPU)'],
-    'RAM': 'Mode/Threads',  # Anta at RAM og Disk har samme identifikator
+    'RAM': 'Mode/Threads',  # RAM og Disk har samme identifikator
     'Disk': 'Mode/Threads'
 }
 
